244084d3738d2da9ba21d93cf62aa2d9c3ea36b0
netvoip <38634548+netvoip@users.noreply.github.com>
2022-12-06 13:37:15 +0300

    code optimization, fixes

--- README.md
+++ README.md
@@ -1,34 +1,29 @@
 ### Introduction
-Retrieves registered phones list from CUCM, connects to each by web interface and parses its serial number. The main advantage is that it can work with any phone model supporting web access (slight code changing might be required). Relatively fast, processing 1000 phones takes less than 25 sec.
+Retrieves registered phones list from CUCM, connects to each by web interface and parses its serial number. The main advantage is that it can work with any phone model with web access enabled. Relatively fast, processing 1000 phones takes 15-30 sec.
 
 Currently tested phone models:
 ```
 Cisco 3905, Cisco 6921, Cisco 6961, Cisco 7811, Cisco 7821, Cisco 7861, Cisco 8841, Cisco 8861
 ```
+If your phone is not listed, slight code changes might be required.
 
 ### Setup
-On client computer you need to have Python interpreter v3.4+. Additional modules must be installed by `pip install zeep` command.  
-Project was migrated to zeep module for SOAP requests. Old version based on suds is still available.
+On client computer you need to have Python interpreter v3.4+. Install required modules by `pip install -r requirements.txt` for console script only or `pip install -r requirements_flask.txt` for using console and web interface.  
 
-`Vars.conf` file should contain: 
+`vars.conf` file should contain: 
 - IP address or hostname of UCM server with "Cisco AXL Web Service" service running, 
 - Username and password of user with "Standard AXL API access" role assigned, 
 - Path to RIS WSDL schema on local computer, which can be downloaded from  
-`https://cucm-ip:8443/realtimeservice2/services/RISService70?wsdl`.
+`https://cucm:8443/realtimeservice2/services/RISService70?wsdl`
 
 ### Usage
-Starting from 9 version, CUCM AXL response is limited to 1000 items. If you have more than 1000 active devices, you need to split your request into smaller ones grouped by directory number, name or IP (only one parameter per request) and model.  
+Starting from 9 version, CUCM AXL response is limited to ~1000 items. If you have more than 1000 active devices, you need to split your request into smaller ones grouped by directory number, name or IP (only one parameter per request) and model.  
 `python get_phones_sn.py -num 1*` - phones with number starting from 1  
 `python get_phones_sn.py -name SEP*` - all hardware phones  
 `python get_phones_sn.py -model 621` - internal Cisco model number is used, see [RisPort70 API Reference](https://developer.cisco.com/docs/sxml/#risport70-api-reference)  
 `python get_phones_sn.py -ip 192.168.* -max 10` - phones in 192.168.0.0/16 network, limit to 10 items  
-`python get_phones_sn.py -noprint` - noprint option is used when you want to minimize screen output. Errors and summary info will be shown anyway.  
 
-Results are saving to _serials.txt file which could be treated as csv and easily converted to xls for further processing.
+Results are printed in csv format so it can be easily converted to xls for further processing.
 
 ### Web GUI
-Here is simple user interface based on Flask. Required packages: Flask, Flask_Bootstrap, Flask_WTF. Run it on WSGI server or locally by command:
-`python flask_start.py`
-
-### P.S.
-If your phone model is not supported or by any other questions contact me by conftdowr@gmail.com.
+Here is simple user interface based on Flask. Run local server by command `python flask_start.py`
--- get_phones_sn.py
+++ get_phones_sn.py
@@ -1,109 +1,197 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 15 11:52:00 2018
-"""
 
 import requests
 import re
+import configparser
 import argparse
 import ssl
+
+from zeep import Client
+from zeep.cache import SqliteCache
+from zeep.transports import Transport
+from zeep.exceptions import Fault
+from zeep.plugins import HistoryPlugin
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from urllib3 import disable_warnings
+from urllib3.exceptions import InsecureRequestWarning
+from lxml import etree
+from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
-from get_active_phones import cucm_rt_phones
 
 Timestart = datetime.now()
 ssl._create_default_https_context = ssl._create_unverified_context
-Printsum = True
-To_file = False
+disable_warnings(InsecureRequestWarning)
+
+vars = configparser.ConfigParser()
+varfile = 'vars.conf'
+vars.read(varfile)
+axluser = vars['cucm']['axluser']
+axlpassword = vars['cucm']['axlpassword']
+cucmhost = vars['cucm']['ip']
+riswsdl = 'https://{}:8443/realtimeservice2/services/RISService70?wsdl'.format(cucmhost)
+
+# Get phones info from cucm
+def cucm_rt_phones(model = 255, name = '', num = '', ip = '', max = 1000, Print = True):
+    StateInfo = ''
+    if name:
+        SelectBy = 'Name'
+        SelectItems = {'item': name}
+    elif num:
+        SelectBy = 'DirNumber'
+        SelectItems = {'item': num}
+    elif ip:
+        SelectBy = 'IPV4Address'
+        SelectItems = {'item': ip}
+    else:
+        SelectBy = 'Name'
+        SelectItems = {}
+    CmSelectionCriteria = {
+        'MaxReturnedDevices': max,
+        'DeviceClass': 'Phone',
+        'Model': model,
+        'Status': 'Registered',
+        'SelectBy': SelectBy,
+        'SelectItems': SelectItems
+    }
+    session = Session()
+    session.verify = False
+    session.auth = HTTPBasicAuth(axluser, axlpassword)
+    transport = Transport(cache=SqliteCache(), session=session, timeout=5)
+    history = HistoryPlugin()
+    client = Client(wsdl=riswsdl, transport=transport, plugins=[history])
+
+    def show_history():
+        for item in [history.last_sent, history.last_received]:
+            print(etree.tostring(item["envelope"], encoding="unicode", pretty_print=True))
+
+    out = []
+    try:
+        resp = client.service.selectCmDevice(CmSelectionCriteria=CmSelectionCriteria, StateInfo=StateInfo)
+        result = resp['SelectCmDeviceResult']['CmNodes']['item']
+        for node in result:
+            if node['CmDevices'] != None:
+                 for device in node['CmDevices']['item']:
+                    current = {}
+                    current['ip'] = device['IPAddress']['item'][0]['IP']
+                    current['model'] = modelname(device['Model'])
+                    current['desc'] = device['Description']
+                    current['num'] = device['DirNumber'].replace('-Registered', '')
+                    out.append(current)
+    except Fault:
+        show_history()
+        return []
+    return out
+
+# Internal model number to human readable
+def modelname(modelnum=0):
+    if modelnum == 336: return('Third-party SIP Device')
+    elif modelnum == 503: return('Cisco Jabber')
+    elif modelnum == 592: return('Cisco 3905')
+    elif modelnum == 36213: return('Cisco 7811')
+    elif modelnum == 621: return('Cisco 7821')
+    elif modelnum == 623: return('Cisco 7861')
+    elif modelnum == 683: return('Cisco 8841')
+    elif modelnum == 685: return('Cisco 8861')
+    elif modelnum == 495: return('Cisco 6921')
+    elif modelnum == 497: return('Cisco 6961')
+    elif modelnum == 622: return('Cisco 7841')
+    elif modelnum == 307: return('Cisco 7911')
+    elif modelnum == 115: return('Cisco 7941')
+    elif modelnum == 434: return('Cisco 7942')
+    elif modelnum == 435: return('Cisco 7945')
+    elif modelnum == 365: return('Cisco 7921')
+    elif modelnum == 484: return('Cisco 7925')
+    elif modelnum == 431: return('Cisco 7937')
+    elif modelnum == 30019: return('Cisco 7936 Conference')
+    elif modelnum == 30007: return('Cisco 7912')
+    elif modelnum == 36255: return('Cisco Spark')
+    else:
+        print('Undefined model name: {}'.format(modelnum))
+        return modelnum
 
 
+# Download phone html page
 def gethtml(url):
-    # Downloads phone html page
     try:
         r = requests.get(url, timeout = 6)
         r.encoding = 'utf-8'
-        Text = r.text
+        out = r.text
     except:
         return('No connect')
-    return(Text)
+    return out
 
 
-def sn(ip, model, num, desc):
-    # Returns dict with serial number
-    Out = {'ip': ip, 'model': model, 'num': num, 'desc': desc}
-    Address = SnRegex = Sn = ''
+# Insert serial number into phone dict
+def extract_sn(ip, model, num, desc):
+    out = {'ip': ip, 'model': model, 'num': num, 'desc': desc}
+    weburl = ''
+    sn_regex = ''
+    sn = ''
     if (model == 'Cisco 7811') or (model == 'Cisco 7861'):
-        Address = 'http://{}/CGI/Java/Serviceability'.format(ip)
-        SnRegex = '</TD><TD><B>([A-Z]{3}\w{8})</B></TD>'
+        weburl = 'http://{}/CGI/Java/Serviceability'.format(ip)
+        sn_regex = '</TD><TD><B>([A-Z]{3}\w{8})</B></TD>'
     elif model == 'Cisco 3905':
-        Address = 'http://{}/Device_Information.html'.format(ip)
-        SnRegex = '<td><p><b>([A-Z]{3}\w{8})</b></p></td>'
+        weburl = 'http://{}/Device_Information.html'.format(ip)
+        sn_regex = '<td><p><b>([A-Z]{3}\w{8})</b></p></td>'
     elif (model == 'Cisco 6961') or (model == 'Cisco 6921'):
-        Address = 'http://{}'.format(ip)
-        SnRegex = '</TD><TD><strong>([A-Z]{3}\w{8})</strong></TD>'
+        weburl = 'http://{}'.format(ip)
+        sn_regex = '</TD><TD><strong>([A-Z]{3}\w{8})</strong></TD>'
     else:
-        Address = 'http://{}'.format(ip)
-        SnRegex = '</TD><TD><B>([A-Z]{3}\w{8})</B></TD>'
+        weburl = 'http://{}'.format(ip)
+        sn_regex = '</TD><TD><B>([A-Z]{3}\w{8})</B></TD>'
 
     if (model == 'Cisco Jabber') or (model == 'Third-party SIP Device'):
-        Sn = 'Not supported'
+        sn = 'Not supported'
     else:
-        Text = gethtml(Address)
+        html = gethtml(weburl)
         try:
-            Sn = re.findall(SnRegex, Text)[0]
+            sn = re.search(sn_regex, html)[1]
         except:
-            Sn = 'Not found'
-    Out['sn'] = Sn
-    return(Out)
+            sn = 'No SN found'
+    out['sn'] = sn
+    return(out)
 
 
 def getphonessn(num, name, ip, max = 30, model=255):
-    Txt = Txttotal = ''
-    Good = Notgood = Notsupported = 0
-    # Get phones list of dicts
-    Devices = []
-    Devices = cucm_rt_phones(model = model, num = num, name = name, ip = ip, max = max, Print = False)
-    Devices = sorted(Devices, key=lambda k: k['ip'])
+    # Get phones as list of dicts
+    devices = []
+    devices = cucm_rt_phones(model = model, num = num, name = name, ip = ip, max = max, Print = False)
+    devices = sorted(devices, key=lambda k: k['ip'])
     # Run sn extraction
     with ThreadPoolExecutor(max_workers=30) as executor:
-        Result = executor.map(sn, (i['ip'] for i in Devices), (j['model'] for j in Devices),
-            (k['num'] for k in Devices), (l['desc'] for l in Devices))
-    Devices_ext = list(Result)
-    # Make fragments of output text
-    for device in Devices_ext:
-        if device['sn'] == 'Not supported':
-            Notsupported += 1
-            continue
-        elif device['sn'] == 'Not found':
-            Notgood += 1
-            Txttotal += '\n' + str(list(device.values()))
-        else:
-            Good += 1
-            Txt += '\n{},{},{},{}'.format(device['model'], device['sn'], device['ip'], device['desc'])
-    # Compose and print output text
-    if Notgood > 0:
-        Txttotal = "\n\nCould not get serial from:" + Txttotal
-    Txttotal += '\n\nSuccessful {}, unsuccessful {}, unsupported {}. Total {}.'.format(Good, Notgood, Notsupported, len(Devices))
-    Txt += Txttotal
-    return(Txt)
+        insert_sn = executor.map(extract_sn, (i['ip'] for i in devices), (j['model'] for j in devices),
+            (k['num'] for k in devices), (l['desc'] for l in devices))
+    devices_with_sn = list(insert_sn)
 
+    # Generate output text
+    out_text = ''
+    summary = ''
+    good = bad = unsup = 0
+    for device in devices_with_sn:
+        if device['sn'] == 'Not supported':
+            unsup += 1
+        elif device['sn'] == 'No SN found':
+            bad += 1
+            summary += str(list(device.values())) + '\n'
+        else:
+            good += 1
+            out_text += '{},{},{},{}\n'.format(device['model'], device['sn'], device['ip'], device['desc'])
+    if bad > 0:
+        summary = "\nCould not get serials from:\n" + summary
+    summary += '\nSuccessful {}, unsuccessful {}, unsupported {}. Total {}.'.format(good, bad, unsup, len(devices))
+    out_text += summary
+    return(out_text)
 
 if __name__ == '__main__':
-    # Parsing arguments
     parser = argparse.ArgumentParser(description='Returns active phone list with serial numbers')
     parser.add_argument('-num', action="store", dest="num", default='')
     parser.add_argument('-name', action="store", dest="name", default='')
     parser.add_argument('-ip', action="store", dest="ip", default='')
     parser.add_argument('-max', action="store", dest="max", default=1500)
     parser.add_argument('-model', action="store", dest="model", default=255)
-    parser.add_argument('-noprint', action="store_false", dest="noprint", default=True)
     args = parser.parse_args()
     Result = getphonessn(num = args.num, name = args.name, ip = args.ip, max = args.max, model = args.model)
-    if args.noprint:
-        print(Result)
-    if Printsum: print('Total time: {}'.format(datetime.now() - Timestart))
-    if To_file:
-        with open('_serials.txt', 'w', encoding='utf-8') as output_file:
-            output_file.write(Result)
-        output_file.close()
+    print(Result)
+    print('Total time: {}'.format(datetime.now() - Timestart))
